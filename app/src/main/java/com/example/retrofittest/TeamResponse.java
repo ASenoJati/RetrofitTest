f7183465f66f40ad51a0b9e9dfcd84df975bbef5
ASenoJati <senojati16@gmail.com>
2025-05-08 11:42:09 +0700

    feat :[SEN] la liga

--- app/src/main/java/com/example/retrofittest/TeamResponse.java
+++ app/src/main/java/com/example/retrofittest/TeamResponse.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class TeamResponse {
-    @SerializedName("teams") // Nama Array di JSON
+    @SerializedName("teams")
     private List<ItemModel> teams;
 
     public List<ItemModel> getTeams() {
